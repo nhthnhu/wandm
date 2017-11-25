3fa47801b9ad1f5d563741986475d04faa677d7e
huuphuoc1396 <huuphuoc1396@gmail.com>
2017-11-25 15:19:39 +0700

    Add speech to text API for SearchActivity

--- wandm/app/src/main/java/com/wandm/activities/SearchActivity.kt
+++ wandm/app/src/main/java/com/wandm/activities/SearchActivity.kt
@@ -2,6 +2,7 @@ package com.wandm.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.speech.SpeechRecognizer
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import android.view.View
@@ -18,9 +19,17 @@ import com.wandm.models.RequestListener
 import com.wandm.models.SongSearchSuggestion
 import com.wandm.models.song.Song
 import com.wandm.models.song.SongRequest
+import com.wandm.permissions.PermissionCallback
+import com.wandm.permissions.askPermission
 import com.wandm.services.MusicPlayer
+import com.wandm.speech.GoogleVoiceTypingDisabledException
+import com.wandm.speech.Speech
+import com.wandm.speech.SpeechDelegate
+import com.wandm.speech.SpeechRecognitionNotAvailable
 import kotlinx.android.synthetic.main.activity_search.*
 import org.jetbrains.anko.doAsync
+import java.util.*
+import kotlin.collections.ArrayList
 
 class SearchActivity : BaseActivity() {
 
@@ -30,6 +39,11 @@ class SearchActivity : BaseActivity() {
     companion object {
         private val TAG = "SearchActivity"
         private val FIND_SUGGESTION_SIMULATED_DELAY = 250L
+        private val SPEECH_LISTENING = "speech_listening"
+        private val SPEECH_DONE = "speech_done"
+        private val SPEECH_ERROR = "speech_error"
+        private val SPEECH_START = "speech_start"
+        private val SPEECH_UPDATE_WAVE = "speech_update_wave"
 
         lateinit var instance: SearchActivity
     }
@@ -44,6 +58,7 @@ class SearchActivity : BaseActivity() {
     override fun initView(savedInstanceState: Bundle?) {
         setupViews()
         setupSearchBar()
+        setupSpeech()
     }
 
     private fun setupViews() {
@@ -129,7 +144,7 @@ class SearchActivity : BaseActivity() {
 
         searchBar.setOnMenuItemClickListener { item ->
             if (item.itemId == R.id.speech_item_menu) {
-
+                startSpeech()
             }
         }
 
@@ -145,6 +160,138 @@ class SearchActivity : BaseActivity() {
 
             if (songSuggestion.isHistory) {
                 leftIcon.setImageDrawable(this.getDrawable(android.R.drawable.ic_menu_recent_history))
+            }
+        }
+    }
+
+    private fun setupSpeech() {
+
+        Speech.init(this)
+        Speech.getInstance().setLocale(Locale.getDefault())
+        Speech.getInstance().setTransitionMinimumDelay(250)
+        Speech.getInstance().setDelegate(object : SpeechDelegate {
+            override fun onStartOfSpeech() {
+                showViewsForSpeech(SPEECH_START)
+            }
+
+            override fun onSpeechRmsChanged(value: Float) {
+                showViewsForSpeech(SPEECH_UPDATE_WAVE, (value / 20).toString())
+            }
+
+            override fun onSpeechPartialResults(results: List<String>) {
+                showViewsForSpeech(SPEECH_LISTENING, results[0])
+            }
+
+            override fun onSpeechResult(result: String) {
+                Log.d(TAG, "SpeechResult: " + result)
+
+                if (result != "") {
+                    searchOffline(result)
+                    searchOnline(result)
+                    showViewsForSpeech(SPEECH_DONE)
+                    searchBar.setSearchText(result)
+                    return
+                }
+            }
+
+            override fun onError(code: Int) {
+                val errorMessage = getErrorText(code)
+                showViewsForSpeech(SPEECH_ERROR, errorMessage)
+            }
+        })
+
+    }
+
+    private fun startSpeech() {
+        val permission = android.Manifest.permission.RECORD_AUDIO
+
+        askPermission(permission, object : PermissionCallback {
+            override fun permissionGranted() {
+
+            }
+
+            override fun permissionRefused() {
+                finish()
+            }
+        })
+
+        if (Speech.getInstance().isListening) {
+            Speech.getInstance().stopListening()
+            return
+        }
+
+        try {
+            Speech.getInstance().startListening()
+        } catch (speechRecognitionNotAvailable: SpeechRecognitionNotAvailable) {
+            Log.e(TAG, "Speech Recognition isn't Available")
+        } catch (e: GoogleVoiceTypingDisabledException) {
+            Log.e(TAG, "Google Voice Typing was disabled")
+        }
+
+    }
+
+    private fun getErrorText(errorCode: Int): String {
+
+        when (errorCode) {
+            SpeechRecognizer.ERROR_AUDIO -> return getString(R.string.error_audio)
+
+            SpeechRecognizer.ERROR_CLIENT -> return getString(R.string.error_client)
+
+            SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS -> return getString(R.string.error_client)
+
+            SpeechRecognizer.ERROR_NETWORK -> return getString(R.string.error_network)
+
+            SpeechRecognizer.ERROR_NETWORK_TIMEOUT -> return getString(R.string.error_network_timeout)
+
+            SpeechRecognizer.ERROR_NO_MATCH -> return getString(R.string.error_no_match)
+
+            SpeechRecognizer.ERROR_RECOGNIZER_BUSY -> return getString(R.string.error_recognizer_busy)
+
+            SpeechRecognizer.ERROR_SERVER -> return getString(R.string.error_server)
+
+            SpeechRecognizer.ERROR_SPEECH_TIMEOUT -> return getString(R.string.error_speech_timeout)
+
+            else -> return getString(R.string.error_default)
+        }
+    }
+
+    private fun showViewsForSpeech(state: String, value: String = "") {
+        when (state) {
+
+            SPEECH_START -> {
+                speechView.visibility = View.VISIBLE
+
+                textListening.text = ""
+                textListening.visibility = View.INVISIBLE
+
+                textMessage.text = ""
+                textMessage.visibility = View.INVISIBLE
+
+                waveformView.reset()
+                waveformView.visibility = View.VISIBLE
+            }
+
+            SPEECH_LISTENING -> {
+                textListening.text = value
+                textListening.visibility = View.VISIBLE
+
+                textMessage.visibility = View.INVISIBLE
+            }
+
+            SPEECH_UPDATE_WAVE -> {
+                waveformView.updateAmplitude(value.toFloat())
+            }
+
+            SPEECH_ERROR -> {
+                textMessage.text = value
+                textMessage.visibility = View.VISIBLE
+
+                textListening.visibility = View.INVISIBLE
+                waveformView.visibility = View.GONE
+            }
+
+            SPEECH_DONE -> {
+                speechView.visibility = View.GONE
             }
         }
     }
